8686d736e2904ff1bd8696c557aefd24f3193c7d
keluthaksash <136364178+keluthaksash@users.noreply.github.com>
2023-06-13 07:01:44 +0530

    Add files via upload

---
+++ ex 23.py
@@ -0,0 +1,6 @@
+import statistics
+sample=[135,137,136,142,135]
+var=statistics.pvariance(sample)
+print("The varience is:",var)
+sd=statiustics.pstdev(sample)
+print("The standard devation is:",sd)
